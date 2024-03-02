6eb7bf187fd5464170148afd0980d0824c5d4ee7
Rodrigo Costa <rodrigocosta@outlook.it>
2024-03-01 23:38:11 -0400

    resolvendo desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iph = new Iphone("(69)99999999","Iphone 11","11143759603941",128);
+iph.Ligar();
+iph.InstalarAplicativo("Instagram");
+iph.ReceberLigacao();
+Console.WriteLine("");
+Nokia nk =new Nokia("(11)99999999","Nokia G60 5G","2334567830495823",256);
+nk.Ligar();
+nk.InstalarAplicativo("credisis");
+nk.ReceberLigacao();
